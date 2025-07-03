7bebf36ec35c5b99402e80104c233192d1cb14d9
PRANSHU PATEL <141228888+PatelPranshu@users.noreply.github.com>
2025-07-03 15:17:17 +0530

    Add files via upload

---
+++ js/popup.js
@@ -0,0 +1,7 @@
+function showPopup() {
+  document.getElementById('popup-details').style.display = 'flex';
+}
+
+function hidePopup() {
+  document.getElementById('popup-details').style.display = 'none';
+}
